60abc16dce93f911889e2e778b151b8480269dee
吉田圭佑 <yoshidakeisuke@tp-kyoshida-mbp.ics.nitech.ac.jp>
2017-11-30 18:33:00 +0900

    random emit gaze data

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-import { setTimeout, setInterval, clearInterval } from "timers";
-
 var app = require("express")();
 var http = require("http").Server(app);
+var io = require('socket.io')(http);
 
 var mConnectingMembers = {};
+var mHeartBeatInterval = null;
+var x = 0;
+var y = 0;
+var vx = 0;
+var vy = 0;
+var ax = 0;
+var ay = 0;
+var screenWidth = 1920;
+var screenHeight = 1200;
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
@@ -19,29 +27,50 @@ io.on('connection', function(socket){
     
   });
 
-  socket.on("disconnect", (event)=>{
+  socket.on("disconnect", function(event){
     delete mConnectingMembers[socket.id];
     if(Object.keys(mConnectingMembers).length == 0){
       stopHeartBeat();
     }
   });
 
-  mConnectingMembers[socket.id] = new Date().getTime();
-  startHeartBeat(broadcastRandomData(socket));
+  socket.on("start_record", function(event){
+    mConnectingMembers[socket.id] = new Date().getTime();
+    startHeartBeat(()=>broadcastRandomData(socket));
+  });
+
 
 });
 
-function broadcastRandomData(socket){
-  var eyeData = {
-    x: 100,
-    y: 100,
-    time: new Date().getTime()
-  }
-  socket.broadcast.emit();
+function updateAcceralate(){
+  ax = Math.random() * 20 - 10;
+  ay = Math.random() * 20 - 10;
 }
 
-var mHeartBeatInterval = null;
-var 
+function updateVelocity(){
+  vx = ax;
+  vy = ay;
+}
+
+function updateMetrics(){
+  x = x + vx;
+  y = y + vy;
+}
+
+function broadcastRandomData(socket){
+  updateAcceralate();
+  updateVelocity();
+  updateMetrics();
+  var eyeData = {
+    x: x,
+    y: y,
+    time: new Date().getTime() - mConnectingMembers[socket.id]
+  }
+  console.log(eyeData);
+  // console.log(socket.broadcast.emit())
+  socket.emit('gazeData',eyeData);
+}
+
 function startHeartBeat(callback){
   if(mHeartBeatInterval){
     clearInterval(mHeartBeatInterval);
